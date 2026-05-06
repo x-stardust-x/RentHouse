c748c0678eaa8d50d2754ac2af8e6dd82da41701
xin26q <qq36148775@gmail.com>
2026-05-06 15:00:16 +0800

    Add rental-matching-component

---
+++ src/app/@component/rental-matching-component/rental-matching-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-matching-component',
+  imports: [],
+  templateUrl: './rental-matching-component.html',
+  styleUrl: './rental-matching-component.scss',
+})
+export class RentalMatchingComponent {}

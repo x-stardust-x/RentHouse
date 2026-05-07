89a267f848cb23c0a3a8f6cb682d41f02a4156b9
x-stardust-x <s8899512345@gmail.com>
2026-05-07 15:34:22 +0800

    新增生日和年齡

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,32 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './@interceptor/auth-interceptor';
+import { provideNativeDateAdapter,MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material/core';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'YYYY/MM/DD',
+  },
+  display: {
+    dateInput: 'yyyy/MM/dd',
+    monthYearLabel: 'yyyy MMM',
+    dateA11yLabel: 'yyyy/MM/dd',
+    monthYearA11yLabel: 'yyyy MMMM',
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideNativeDateAdapter(),{
+      provide: MAT_DATE_LOCALE,
+      useValue: 'zh-TW'
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: MY_DATE_FORMATS
+    }
   ]
 };

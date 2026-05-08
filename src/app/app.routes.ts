0cc7b3033756c7cc9a43db9a6b444a6b9160df0f
xin26q <qq36148775@gmail.com>
2026-05-08 09:33:34 +0800

    路由變動

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes  } from '@angular/router';
+import { Routes } from '@angular/router';
 import { authGuard } from './@guard/auth-guard';
 import { loginGuard } from './@guard/login-guard';
 
@@ -6,39 +6,39 @@ export const routes: Routes = [
   {
     path: 'home',
     // redirectTo: 'home',
-    loadComponent : () => import('./@component/home-component/home-component').then(c => c.HomeComponent),
+    loadComponent: () => import('./@component/home-component/home-component').then(c => c.HomeComponent),
     pathMatch: 'full'
   },
   {
-    path:'login',
-    loadComponent :() => import('./@component/login-component/login-component').then(c => c.LoginComponent),
-    canActivate : [loginGuard]
+    path: 'login',
+    loadComponent: () => import('./@component/login-component/login-component').then(c => c.LoginComponent),
+    canActivate: [loginGuard]
   },
   {
-    path:'member',
-    loadComponent :() => import('./@component/member-component/member-component').then(c => c.MemberComponent),
-    canActivate : [authGuard],
-    data:{roles : ['member']},
-    children:[],
+    path: 'member',
+    loadComponent: () => import('./@component/member-component/member-component').then(c => c.MemberComponent),
+    canActivate: [authGuard],
+    data: { roles: ['member'] },
+    children: [],
   },
   {
-    path:'admin',
-    loadComponent :() => import('./@component/admin-component/admin-component').then(c => c.AdminComponent),
-    canActivate : [authGuard],
-    data:{roles : ['admin']},
-    children:[],
+    path: 'admin',
+    loadComponent: () => import('./@component/admin-component/admin-component').then(c => c.AdminComponent),
+    canActivate: [authGuard],
+    data: { roles: ['admin'] },
+    children: [],
   },
   {
-    path:'register',
-    loadComponent :() => import('./@component/register-component/register-component').then(c => c.RegisterComponent),
-    canActivate : [loginGuard]
+    path: 'register',
+    loadComponent: () => import('./@component/register-component/register-component').then(c => c.RegisterComponent),
+    canActivate: [loginGuard]
   },
   {
-    path:'rental-matching-component',
-    loadComponent :() => import('./@component/rental-matching-component/rental-matching-component').then(c => c.RentalMatchingComponent),
+    path: 'rental-matching-component',
+    loadComponent: () => import('./@component/rental-matching-component/rental-matching-component').then(c => c.RentalMatchingComponent),
   },
   {
-    path:'**',
+    path: '**',
     // loadComponent :() => import('./@component/login-component/login-component').then(c => c.LoginComponent),
     redirectTo: 'home'
   },

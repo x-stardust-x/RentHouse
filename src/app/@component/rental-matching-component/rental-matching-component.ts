8b0e71674e8a4ac782f0aff0d248fe968ba07fa4
xin26q <qq36148775@gmail.com>
2026-05-12 16:48:38 +0800

    房屋租賃列表資料串接

--- src/app/@component/rental-matching-component/rental-matching-component.ts
+++ src/app/@component/rental-matching-component/rental-matching-component.ts
@@ -1,20 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RentalMatchingService } from '../../@service/rental-matching-service';
+import { MatchHouseDto } from '../../@interface/match-house';
+import { CommonModule } from '@angular/common';
+// import { RentalService } from
+
 
 @Component({
   selector: 'app-rental-matching-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rental-matching-component.html',
   styleUrl: './rental-matching-component.scss',
 })
-export class RentalMatchingComponent {
+
+export class RentalMatchingComponent implements OnInit {
+
+  houses = signal<MatchHouseDto[]>([]);
+
   city: string = '';
   rentalItemCount: number = 0;
   viewMode: 'grid' | 'map' = 'grid';
 
-  constructor(private router: Router) {}
+  constructor(private rentalMatchingService: RentalMatchingService, private router: Router) {}
 
-  navigateToDetail(id: string) {
-    this.router.navigate(['/rental-matching-detail', id]);
+  ngOnInit(): void {
+    this.rentalMatchingService.getRentals().subscribe({
+      next: (data) => {
+        this.houses.set(data);
+        this.rentalItemCount = data.length;
+        console.log('成功抓取房屋資料！', this.houses());
+      },
+      error: (err) => console.error('抓取資料失敗', err)
+    });
   }
+
+  navigateToDetail(id: number | undefined): void {
+    if (id) {
+      this.router.navigate(['/rental-matching-detail', id]);
+    } else {
+      console.warn('找不到該房屋的 ID');
+    }
+
+  }
+
+  // items = this.houses;
 }
--- src/app/@service/rental-matching-service.ts
+++ src/app/@service/rental-matching-service.ts
@@ -1,17 +1,32 @@
+import { MatchHouseDto } from './../@interface/match-house';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class RentalMatchingService {
-  city: string = '';
-  rentalItemCount: number = 0;
+  private apiUrl = 'https://localhost:7215/api/RentalMatching';
+
+  // city: string = '';
+  // rentalItemCount: number = 0;
 
   constructor(private http: HttpClient) {
-    this.http.get('api/rental/info').subscribe((data: any) => {
-      this.city = data.city;
-      this.rentalItemCount = data.rentalItemCount;
-    });
+    // this.http.get('api/rental/info').subscribe((data: any) => {
+    //   this.city = data.city;
+    //   this.rentalItemCount = data.rentalItemCount;
+    // });
+  }
+
+  // 取得全部上架房屋
+  getRentals(): Observable<MatchHouseDto[]> {
+    return this.http.get<MatchHouseDto[]>(this.apiUrl);
+  }
+
+  // 依 ID 取得單一房屋
+  getRentalById(id: number): Observable<MatchHouseDto> {
+    return this.http.get<MatchHouseDto>(`${this.apiUrl}/${id}`);
   }
 }

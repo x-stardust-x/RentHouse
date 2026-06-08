86bf290ef5c6d4a8c2ab8bb2cd932dc7c86b11ff
x-stardust-x <s8899512345@gmail.com>
2026-06-08 11:01:09 +0800

    add page for faq news users

--- src/app/@component/@admin/news-component/news-component.ts
+++ src/app/@component/@admin/news-component/news-component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, SimpleChange } from '@angular/core';
+import { Component, effect, inject, signal, computed } from '@angular/core';
 import { NewsService, } from '../../../@service/news-service';
 import { DatePipe, NgClass } from '@angular/common';
 import { RouterLink } from "@angular/router";
@@ -17,16 +17,36 @@ export class NewsComponent {
   public newsev = inject(NewsService);
   public authsev = inject(Authservice);
   private logsev = inject(LogService);
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-  }
+  currentPage = signal(1);
+  pageSize = signal(5);
+
   constructor() {
     this.newsev.getAll();
-    effect(() => {
 
-    })
   }
+
+  pagedNews = computed(() => {
+    const data = this.newsev.newsData();
+
+    const start = (this.currentPage() - 1) * this.pageSize();
+    const end = start + this.pageSize();
+
+    return data.slice(start, end);
+  });
+
+  totalPages = computed(() => {
+    return Math.ceil(this.newsev.newsData().length / this.pageSize());
+  });
+
+  pages = computed(() => {
+    return Array.from({ length: this.totalPages() }, (_, i) => i + 1);
+  });
+
+  changePage(page: number) {
+    if (page < 1 || page > this.totalPages()) return;
+    this.currentPage.set(page);
+  }
+
   deleteNews(id: number) {
 
     if (!confirm('確定要刪除嗎？')) return;
@@ -56,6 +76,8 @@ export class NewsComponent {
 
         // 重新取得列表
         this.newsev.getAll();
+
+        this.currentPage.set(1); // ⭐ 避免刪完頁數錯亂
 
       },
 
